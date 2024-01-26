2c3064bec1f2a744e6cd046050089d9fed265a81
Aaren Vo <aarenjvo@gmail.com>
2024-01-26 10:23:52 -0800

    extended new class named 'Car' +  wrote new methods & properties

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,9 +1,87 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicleBaseClass").Vehicle
+
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passengers = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    checkService(mileage) {
+
+        this.mileage = (Math.random() * 80000);
+
+
+        if (this.mileage > 30000) {
+            console.log('time for maintenence');
+            return this.scheduleService = true;
+        } else {
+            console.log('your car is up to date!');
+            return this.scheduleService = false;
+        }
+    }
+
+
+    // Get creative and create this method on your own.
+    loadPassenger(num) {
+
+        this.passengers = (Math.random() * 10);
+
+        if (this.passengers > this.maxPassengers) {
+            console.log('max capacity reached, not safe to drive!');
+            return this.started = true;
+        } else {
+            console.log('sensing no capacity issues, drive safe!');
+            return this.started = false;
+
+        }
+
+
+    }
+
+
+    start() {
+
+        this.fuel = (Math.random() * 10);
+
+        if (this.fuel > 5) {
+            console.log('engine started.');
+            return this.started = true;
+        } else {
+            console.log('no fuel to start.');
+            return this.started = false;
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+let newCar = new Car("Mercury", "Sedan", "1965", "Black", "31000");
+
+newCar.start()
+newCar.stop()
+newCar.checkService()
+newCar.loadPassenger()
+
+console.log(newCar)
 
 
 //After you write the derived Car class, you should test it out.
